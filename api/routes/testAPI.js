5c5540049310f433a212b96cbbe95df05f8c3a91
noahjacknichols <noahjacknichols@gmail.com>
2020-04-26 15:25:02 -0400

    added connection to database

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,30 @@
 var express = require("express");
 var router = express.Router();
+const mysql = require('mysql');
+const con = mysql.createConnection({
+    host: 'database-2.ckb7qqal4m5l.us-east-2.rds.amazonaws.com',
+    user: 'root',
+    password: 'password',
+    database: 'STUPID_SHIT'
+  });
+
+var x;
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM users", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+      x = result;
+    });
+  })
+
+  
+
+
 
 router.get("/", function(req,res,next){
-    res.send("API is working properly");
+    res.send(x);
 });
 
 module.exports = router;
